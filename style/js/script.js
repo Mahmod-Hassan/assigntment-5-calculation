7cce7760ff75916b7160149e31904a282e19f869
Mahmod Hassan <mahmodhasan7788@gmail.com>
2022-08-22 21:01:04 +0600

    click button and show on order lish

--- style/js/script.js
+++ style/js/script.js
@@ -1,3 +1,22 @@
 document.getElementById("card-container").addEventListener('click', function (event) {
+    const button = event.target;
+    if (button.tagName === 'BUTTON') {
+        const ol = document.getElementById('player-list');
+        const playerName = event.target.parentElement.firstElementChild.innerText;
+        const li = document.createElement('li');
+        li.innerText = playerName;
+        li.style.fontSize = '25px';
+        console.log(ol.childElements);
+        if (ol.childNodes.length > 5) {
+            alert('you can not added more player');
+            return;
+        }
+        else {
+            ol.appendChild(li);
+            button.setAttribute('disabled', true);
+            button.style.backgroundColor = '#B3B3B3';
+        }
 
+
+    }
 });
